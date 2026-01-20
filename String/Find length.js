0be347f7233e391ff57eb82497f8536a9aee91db
vaibhavjagtap96 <jagtapvaibhav946@gmail.com>
2026-01-20 23:07:36 +0530

    add string folder

---
+++ String/Find length.js
@@ -0,0 +1,45 @@
+let str = "HelloWorld";
+let count = 0;
+
+for (let i = 0; str[i] !== undefined; i++) {
+    count++;
+}
+
+console.log("Length of string:", count);
+
+// -----------------------------------------------------
+
+// let str = "JavaScript";
+// let count = 0;
+
+// for (let ch of str) {
+//     count++;
+// }
+
+// console.log("Length of string:", count);
+
+
+// -------------------
+
+// let str = "Hello World";
+// let count = 0;
+
+// while (str[count] !== undefined) {
+//     count++;
+// }
+
+// console.log(count);
+
+// ----------------------
+
+// let str = "Hello World";
+// let length = 0;
+
+// for (let i = 0; ; i++) {
+//     if (str[i] === undefined) {
+//         break;
+//     }
+//     length++;
+// }
+
+// console.log(length);
